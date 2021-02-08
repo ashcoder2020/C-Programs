f6797ca86f1217fa866c4ddc5a0a030ae0cf29ad
Ashish Meshram <54635178+ashcoder2020@users.noreply.github.com>
2021-02-08 17:05:28 +0530

    Create function_with_return_type.cpp

---
+++ function_with_return_type.cpp
@@ -0,0 +1,26 @@
+//function with return type
+
+
+#include <iostream>
+
+using namespace std;
+
+int Abc(){
+    cout<<"Function with return type int"<<endl;
+    
+    return 1;
+}
+
+int main()
+{
+    int x;
+  
+    x = Abc();
+    cout<<x;
+
+    return 0;
+    
+    
+}
+
+
