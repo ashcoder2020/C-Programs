14db9640398498892b3c79c5d9f214515c2e36c5
Ashish Meshram <54635178+ashcoder2020@users.noreply.github.com>
2021-02-08 23:31:58 +0530

    Create simple_if_else.cpp

---
+++ simple_if_else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i;
+   cout<<"Enter value of i :";
+   cin>>i;
+   if(i==0){
+       cout<<"zero";
+   }
+    else{
+        cout<<"Non Zero";
+    }
+    return 0;
+}
