67b4a95478acb0294e0dc5834648558f167a2737
Ashish Meshram <54635178+ashcoder2020@users.noreply.github.com>
2021-02-08 17:50:46 +0530

    Create simple_check even_odd.cpp

---
+++ simple_check even_odd.cpp
@@ -0,0 +1,19 @@
+//find even odd without class
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number to check even odd : ";
+    cin>>n;
+    if((n%2)==0){
+        cout<<"Even";
+    }
+    else{
+        cout<<"Odd";
+    }
+
+    return 0;
+}
