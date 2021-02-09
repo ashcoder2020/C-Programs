49d29870e03b92b773577a34e6926efcafb58b03
Ashish Meshram <54635178+ashcoder2020@users.noreply.github.com>
2021-02-09 15:43:45 +0530

    Create function_overriding1.cpp

---
+++ function_overriding1.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+class Abc{
+  public:
+  
+  void fun(){
+      cout<<"Abc class fun"<<endl;
+  }
+
+};
+class Def:public Abc{
+    public:
+    void fun(){
+        Abc :: fun();
+        cout<<"Def class fun"<<endl;
+    }
+    
+};
+
+
+
+int main()
+{
+    Def obj;
+    obj.fun();
+
+    return 0;
+}
