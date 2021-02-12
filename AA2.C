a990f2d69d2406c2b2b10bd233663a6499f8e796
Ashish Meshram <54635178+ashcoder2020@users.noreply.github.com>
2021-02-12 17:24:51 +0530

    copy one file data to another file

--- AA2.C
+++ AA2.C
@@ -1,13 +1,23 @@
 #include<stdio.h>
 #include<conio.h>
 #include<iostream.h>
-
+#include<fstream.h>
 
 void main(){
+
+char str[3000];
 clrscr();
 
+fstream firstfileopen("file1.txt",ios::in);
+fstream secondfilewrite("file2.txt",ios::out);
 
+while(!firstfileopen.eof())
+{
 
+ firstfileopen.getline(str,2000);
+ cout<<"first file data read:"<<str<<endl;
+ secondfilewrite<<str<<endl;
+}
 
 getch();
 }
