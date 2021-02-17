2452f082cd1b8792f9e61e8e67f7036d426f2e27
Ashish Meshram <54635178+ashcoder2020@users.noreply.github.com>
2021-02-17 22:05:55 +0530

    Create pattern5.cpp

---
+++ pattern5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int i,j,k,s=7,n;
+   char nn;
+
+for (int i=0;i<5;i++)
+{
+   for (int k=0;k<s;k++)
+{
+   cout<<" ";
+}
+n=64+i*2-1;
+
+for (int j=0;j<2*i-1;j++)
+{
+    nn=n;
+   cout<<nn;
+   n--;
+
+
+}
+   s--;
+   cout<<endl;
+}
+return 0;
+}
